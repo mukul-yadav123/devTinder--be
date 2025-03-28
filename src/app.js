7b92ae83ddabf48cb752341cd5e9a48ac368d10a
Mukul Yadav <mukuly2002@gmail.com>
2025-03-28 15:41:23 +0530

    User model added

--- src/app.js
+++ src/app.js
@@ -1,14 +1,33 @@
 const express = require('express');
-// import {express} from 'express'
-
+const {connectDb}= require('./config/database')
+const User = require('./models/user')
 const app = express();
 
+app.post('/signup', async(req,res) => {
+    const userObj = {
+        firstName: 'Mukul',
+        lastName: 'Yadav',
+        emailId: 'mukuly2002@gmail.com',
+        age: 22,
+        password: '12345678'
+    }
 
-app.use("/test",(req,res) => {
-res.send('Hello WOrld from 3000')
+    const user = new User(userObj);
+    try{
+        await user.save()
+        res.send('User added successfully')
+    }
+    catch(err){
+        res.status(400).send('Error saving the user' + err.message)
+    }
+
 })
 
-
-app.listen(3000,() => {
-    console.log('server is listening on port 3000')
-})
+connectDb()
+    .then(() => {
+        console.log('Database connected')
+        app.listen(3000,() => {
+            console.log('server is listening on port 3000')
+        })
+    })
+    .catch((err) => console.error('Database cannot be connected'))
